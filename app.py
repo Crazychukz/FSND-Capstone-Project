bb8c5d751ab0407d7ca7872b636915e13199f96a
crazychukz <chukwuemekacharles88@gmail.com>
2021-03-24 18:19:57 +0100

    updated app.py

--- app.py
+++ app.py
@@ -268,9 +268,9 @@ def create_app(test_config=None):
         }), error.status_code
 
     if __name__ == '__main__':
-        APP.run(host='0.0.0.0', port=8080, debug=True)
+        app.run(host='0.0.0.0', port=8080, debug=True)
 
     return app
 
 
-APP = create_app()
+app = create_app()
